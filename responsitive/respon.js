9f9f87add127551de1aface8f1470604c544cb9f
wgcv <gstavocevallos@gmail.com>
2014-11-09 23:47:44 -0500

    Se creo una carpeta organizar template

---
+++ responsitive/respon.js
@@ -0,0 +1,14 @@
+
+function reponsitive(){
+
+$("#ocultarMenu").click(function(){
+$("#menuM").hide();
+$("nav").show();
+});
+$("#mostrarMenu").click(function(){
+$("#menuM").show();
+$
+
+$("nav").hide();
+});
+}
